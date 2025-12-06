7099aaf7ce2d57648e86bf7128c6ec5877fd1096
DBappa <bappadityadutta2024@gmail.com>
2025-12-06 11:20:53 +0530

    Back Button Implemented

--- job-portal/src/Pages/SignUpPages.tsx
+++ job-portal/src/Pages/SignUpPages.tsx
@@ -1,13 +1,16 @@
-import { IconAnchor } from "@tabler/icons-react";
+import { IconAnchor, IconArrowLeft } from "@tabler/icons-react";
 import React from "react";
 import SignUp from "../SignUpLogin/SignUp";
 import Login from "../SignUpLogin/Login";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Button } from "@mantine/core";
 
 const SignUpPages = () => {
     const location=useLocation();
+    const navigate= useNavigate();
   return (
-    <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] overflow-hidden">
+    <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins'] overflow-hidden relative">
+      <Button className="!absolute left-5 z-10" size="sm" onClick={()=>navigate("/")} my="lg" color="bright-sun.4" leftSection={<IconArrowLeft size={20}/>} variant="light">Home</Button>
       <div className={`w-[100vw] h-[100vh] transition-all easy-in-out duration-1000 flex [&>*]:flex-shrink-0 ${location.pathname==='/signup'?'-translate-x-1/2':'translate-x-0'}` }>
         <Login/>
         <div className={`w-1/2 h-full transition-all easy-in-out duration-1000 ${location.pathname==='/signup'?'rounded-r-[200px]':'rounded-l-[200px]'}  bg-mine-shaft-900 flex items-center gap-5 justify-center flex-col`}>
