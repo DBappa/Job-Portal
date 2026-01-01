3b1db6c9c26ef240338a5ff23cd98a7472c9d92d
DBappa <bappadityadutta2024@gmail.com>
2026-01-01 23:33:03 +0530

    VerifyOTP and Change Password

--- job-portal/src/SignUpLogin/ResetPassword.tsx
+++ job-portal/src/SignUpLogin/ResetPassword.tsx
@@ -1,18 +1,25 @@
-import { Button, Modal, PinInput, TextInput } from "@mantine/core";
-import { IconAt } from "@tabler/icons-react";
+import { Button, Modal, PasswordInput, PinInput, TextInput } from "@mantine/core";
+import { IconAt, IconLock } from "@tabler/icons-react";
 import React, { useState } from "react";
 import { sendOtp, verifyOtp } from "../Services/UserService";
+import { signupValidation } from "../Services/FormValidation";
+import { successNotification } from "../Services/NotificationService";
 
 const ResetPassword = (props: any) => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passError, setPassError] = useState("");
   const [otpSent, setOtpSent] = useState(false);
   const [otpSending, setOtpSending] = useState(false);
+  const [verified, setVerified] = useState(false);
 
   const handleSendOtp = () => {
     setOtpSending(true);
     sendOtp(email)
       .then((res) => {
+        successNotification("OTP Sent", "Please check your email for the OTP.");
         setOtpSent(true);
+        setOtpSending(false);
       })
       .catch((err) => {
         console.log(err);
@@ -27,13 +34,35 @@ const ResetPassword = (props: any) => {
     verifyOtp(email, value)
       .then((res) => {
         console.log(res);
+        setVerified(true);
       })
       .catch((err) => {
         console.log(err);
       });
-    
   };
 
+  const resendOtp = () => {
+    setOtpSending(true);
+    sendOtp(email)
+      .then((res) => {
+        setOtpSent(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setOtpSent(false);
+        setOtpSending(false);
+      });
+  };
+
+  const changeEmail = () => {
+    setOtpSent(false);
+    setEmail("");
+  };
+
+  const handleResetPassword = () => {
+    
+    console.log(`Resetting password for ${email} to ${password}`);
+  };
 
   return (
     <>
@@ -59,7 +88,9 @@ const ResetPassword = (props: any) => {
                   disabled={email === "" || otpSent}
                   color="bright-sun.4"
                   variant="filled"
-                >Email</Button>
+                >
+                  Email
+                </Button>
               }
               rightSectionWidth="xl"
             />
@@ -74,6 +105,53 @@ const ResetPassword = (props: any) => {
                 inputMode="text"
                 onComplete={handleVerifyOtp}
               />
+            )}
+            {otpSent && !verified && (
+              <div className="flex gap-2">
+                <Button
+                  fullWidth
+                  loading={otpSending}
+                  onClick={resendOtp}
+                  autoContrast
+                  color="bright-sun.4"
+                  variant="light"
+                >
+                  Email
+                </Button>
+                <Button
+                  fullWidth
+                  onClick={changeEmail}
+                  autoContrast
+                  color="bright-sun.4"
+                  variant="filled"
+                >
+                  Change Email
+                </Button>
+              </div>
+            )}
+            {verified && (
+              <PasswordInput
+                name="password"
+                withAsterisk
+                leftSection={<IconLock size={18} stroke={1.5} />}
+                label="Password"
+                placeholder="Password"
+                error={passError}
+                value={password}
+                onChange={(e)=>{setPassword(e.target.value);setPassError(signupValidation("password",e.target.value,""))}}
+              />
+            )}
+            {verified && (
+              <Button
+                fullWidth
+                onClick={handleResetPassword}
+                autoContrast
+                disabled={passError!=="" || password===""}
+                color="bright-sun.4"
+                variant="filled"                
+              >
+                Reset Password
+              </Button>
             )}
           </div>
         }
