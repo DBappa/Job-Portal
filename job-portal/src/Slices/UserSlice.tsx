42333cc0b2f127e09fa0c66db0d399f22cd6b9d4
DBappa <bappadityadutta2024@gmail.com>
2026-01-03 11:23:03 +0530

    Redux Basic Slice Implementation Started.

---
+++ job-portal/src/Slices/UserSlice.tsx
@@ -0,0 +1,21 @@
+import { getItem, setItem } from "../Services/LocalStorageService";
+import { createSlice } from "@reduxjs/toolkit";
+
+const UserSlice = createSlice({
+  name: "user",
+  initialState: getItem("user"),
+    reducers: {
+        setUser:(state,action)=>{
+            setItem("user",action.payload);
+            state=getItem("user");
+            return state;
+        },
+        removeUser:(state)=>{
+            localStorage.removeItem("user");
+            state=null;
+            return state;
+        }
+    }
+});
+
+export const {setUser,removeUser}=UserSlice.actions;
